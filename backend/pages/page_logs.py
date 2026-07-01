4fbd5ffeb943ae83b73c0dcb9da5fb9235d423f6
Sofia Sokolova <sofi.sokolova@innopolis.university>
2026-07-01 23:20:56 +0300

    feat: add filtering by date range

--- backend/pages/page_logs.py
+++ backend/pages/page_logs.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import sys
 import os
+import datetime
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
@@ -16,9 +17,17 @@ st.markdown(
 
 logs = get_all_logs()
 df = pd.DataFrame(logs)
+filter_criteria = st.date_input(
+    "Filter by the range of dates",
+    (datetime.date.today() - datetime.timedelta(days=2), datetime.date.today()),
+    min_value=datetime.date.today() - datetime.timedelta(days=2),
+    max_value=datetime.date.today(),
+    format="DD.MM.YYYY",
+)
 if not df.empty:
     df["time"] = (pd.to_datetime(df["time"]).dt.tz_localize("UTC").dt.tz_convert("Europe/Moscow").dt.tz_localize(None))
-
+    if len(filter_criteria) == 2:
+        df = df[(df["time"] > (pd.Timestamp(filter_criteria[0]) - datetime.timedelta(days=1))) & (df["time"] < (pd.Timestamp(filter_criteria[1] + datetime.timedelta(days=1))))]
 st.dataframe(
     df,
     column_order=["id", "name", "time", "status"],
